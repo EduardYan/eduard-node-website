09d3977973dffa093af9b233c15a7a09e7769823
EduardYan <eduarygp@gmail.com>
2023-08-09 18:22:35 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# NODE WEBSITE
+
+## A simple website using express framework.
+![screenshot](./doc/images/screenshot.png)
---
+++ index.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+
+const port = process.env.PORT || 3000;
+const templatesPath = path.join("public/");
+
+const app = express();
+
+//static files
+app.use(express.static("/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(templatesPath + "index.html", {
+    root: __dirname,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(templatesPath + "about.html", {
+    root: __dirname,
+  });
+});
+
+app.get("/test/:verify", (req, res) => {
+  if (req.params.verify === "True") {
+    res.sendFile(templatesPath + "/test/page.html", {
+      root: __dirname,
+    });
+  } else {
+    res.sendStatus(404);
+  }
+  
+});
+
+app.listen(port);
+console.log(`Server in port ${port}`);
